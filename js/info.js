26f974a32b062461b4a0aa9a7a73980078192096
Michal Zimmermann <zimmicz@gmail.com>
2016-11-08 18:06:13 +0100

    Add more info

--- js/info.js
+++ js/info.js
@@ -5,8 +5,11 @@ Monitor.Info = {
     _listenDatachange: function() {
         document.querySelector(".info").addEventListener("datachange", (e) => {
             let cadastre = Monitor.Data.getCurrent();
+            let avgArea = parseInt(cadastre.celkova_vymera / cadastre.celkovy_pocet_parcel);
             document.querySelector(".info small")
-                .innerHTML = `výměra: ${(cadastre.celkova_vymera/10000).toFixed(2)} ha / počet parcel: ${cadastre.celkovy_pocet_parcel}`;
+                .innerHTML = `výměra: ${(cadastre.celkova_vymera/10000).toFixed(2)} ha /
+                    počet parcel: ${cadastre.celkovy_pocet_parcel} /
+                    průměrná výměra parcely: ${avgArea} m<sup>2</sup>`;
         });
     }
 }
